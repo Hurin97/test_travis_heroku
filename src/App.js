6cf191bc38c6d9f3451b1ab32752beec112789c7
Max <maxtyanigin97@mail.ru>
2020-02-10 05:57:03 +0300

    test

--- src/App.js
+++ src/App.js
@@ -8,16 +8,17 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Wake up,Neo... The Matrix has you...
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=Smwrw4sNCxE"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Follow the white rabbit...
         </a>
+
       </header>
     </div>
   );
